c4a4945df34d928c2a45c8e954a7504acb9fc641
NikkiMcMistie <storagefornikkione@gmail.com>
2024-09-07 20:36:38 +0200

    w
    w

---
+++ xsh/infosoosage/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    /*ELEMENT CONTENT - TEMPLATE ELEMENT NAME*/
+    loadHTML('templates/footerblock.html', 'TEMPLATE-IS-FOOTER-BLOCK');
+    loadHTML('templates/topbarblock.html', 'TEMPLATE-TOPBAR-BLOCK');
+
+
+
+});
